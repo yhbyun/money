3215a88af76afd64220684a59edb8da3c8f31942
vagrant <vagrant@vaprobash.dev>
2014-04-29 08:55:35 +0000

    CRUD, AUTO_Items Complategit ad

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,53 @@
 'use strict';
 
 angular.module('moneyApp').
-  controller('MainCtrl', function($scope, $http, $filter, $q, ngTableParams) {
+  controller('MainCtrl', function($scope, $http, ngTableParams) {
   var data = [];
-    $http.get('/api/v1/moneybooks').then(function(result) {
-        $scope.data = result.data.results.data;
-        data = $scope.data;
-    });
+  var itemdata = [];
 
-  $scope.enter = function($kk, $ss) {
-    if($kk !== "" && $ss !== "") {
-      
-    } else{
-       
-    }
+   //리스트
+   $scope.init = function () {
+   $http.get('/api/v1/moneybooks/').then(function(result) {
+      $scope.data = result.data.results.data;
+      data = $scope.data;
+    });
+  };
+
+  //등록
+  $scope.add = function($data) {
+    var data = $data;
+    $http.post('/api/v1/moneybooks/', data).success(function(result) {
+      data.item = '';
+      data.amount = '';
+      data.date = '';
+    $scope.init(); //페이지 리로드  
+    });
+  };
+
+  //업데이트
+  $scope.update = function($data) {
+    var data = $data;
+    $http.put('/api/v1/moneybooks/'+ data.id, data).success(function(result) {
+      $scope.init(); //페이지 리로드  
+    });
+  };
+
+  //삭제
+  $scope.delete = function($data) {
+    var data = $data;
+    $http.delete('/api/v1/moneybooks/'+ data).success(function(result) {
+      $scope.init(); //페이지 리로드  
+    });
+  };
+
+  //자동완성
+  $scope.items = function($data) {
+    var data = $data;
+    $http.get('/api/v1/auto-items/?q='+ data).then(function(result) {
+      //console.log('[auto-items] result='+JSON.stringify(result));
+      $scope.itemdata = result.data.results;
+      itemdata = $scope.itemdata;
+    });
   };
 
  $scope.moneyBooks = new ngTableParams({
@@ -22,10 +56,10 @@ angular.module('moneyApp').
     sorting: {
       date: 'desc'     // initial sorting
     }
-    }, {
+   }, { 
     total: data.length, // length of data
-    getData: function ($defer, params) {
-        $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-      }
+      getData: function($defer, params) {
+      $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+    }
   });
 });
